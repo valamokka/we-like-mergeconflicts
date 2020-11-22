57bdffbc60b6a18e51a927aa3827eb2028dbcb2c
Marton Szabo <szabm78@gmail.com>
2020-11-22 17:14:37 +0100

    please be more cautious :D

--- src/main/java/ConflictingMerges.java
+++ src/main/java/ConflictingMerges.java
@@ -1,7 +1,7 @@
 public class ConflictingMerges {
 
     public static void main() {
-        System.out.println("Hello Marci");
+        System.out.println("Hello Marton"); // Names!! :D
         System.out.println("Hello Odi");
         System.out.println("Hogy vagy?");
     }
