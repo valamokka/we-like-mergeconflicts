98c1bfbc1fe4d0dd3c8c9af81926fa59f1913932
bartaodi <barta.kod@gmail.com>
2020-11-22 17:02:26 +0100

    input added

--- src/main/java/ConflictingMerges.java
+++ src/main/java/ConflictingMerges.java
@@ -3,6 +3,7 @@ public class ConflictingMerges {
     public static void main() {
         System.out.println("Hello Marci");
         System.out.println("Hello Odi");
+        System.out.println("Hogy vagy?");
     }
 
 }
